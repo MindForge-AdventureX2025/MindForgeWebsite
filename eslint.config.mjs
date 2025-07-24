2807401e6f2b54d88f216875f299ecaff14c7f86
liangmiQwQ <mi_yyds@126.com>
2025-07-24 09:53:16 +0800

    feat: installed tailwindcss & shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
   // Your custom configs here
-)
+  // 防止.vue文件名称只有一个单词而报错 (nuxt项目允许 default.vue文件名)
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  }
+);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
+  content: {
+    experimental: { nativeSqlite: true },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/tailwind.css"],
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "shadcn-nuxt",
+  ],
+  shadcn: {},
+});
