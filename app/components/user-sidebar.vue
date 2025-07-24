4c94ad06d19b1dcda5ae30163d13bedc687bb230
liangmiQwQ <mi_yyds@126.com>
2025-07-24 14:35:34 +0800

    feat: add side bar

--- app/components/user-sidebar.vue
+++ app/components/user-sidebar.vue
@@ -1,9 +1,45 @@
 <template>
   <ClientOnly>
-    <Sidebar>
-      <SidebarHeader></SidebarHeader>
-      <SidebarContent></SidebarContent>
-      <SidebarGroup></SidebarGroup>
+    <Sidebar collapsible="icon">
+      <SidebarHeader> <SidebarTrigger /> S**t</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel> F </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild class="flex items-center">
+                  <NuxtLink>
+                    <Icon name="arcticons:human-benchmark" class="text-xl" />Ryan Zhu
+                  </NuxtLink>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild class="flex items-center">
+                  <NuxtLink>
+                    <Icon name="arcticons:human-benchmark" class="text-xl" />R**n Z*u
+                  </NuxtLink>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild class="flex items-center">
+                  <NuxtLink>
+                    <Icon name="arcticons:human-benchmark" class="text-xl" />R**n Z*u
+                  </NuxtLink>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild class="flex items-center">
+                  <NuxtLink>
+                    <Icon name="arcticons:human-benchmark" class="text-xl" />R**n Z*u
+                  </NuxtLink>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>Adventure X</SidebarFooter>
     </Sidebar>
   </ClientOnly>
 </template>
