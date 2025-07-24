9d58c6cc849395379548d9c5b80de7b9c5f769c0
liangmiQwQ <mi_yyds@126.com>
2025-07-24 18:02:23 +0800

    feat: add user's button

--- app/components/user-app-bar.vue
+++ app/components/user-app-bar.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="w-full h-14 flex items-center justify-between px-5">
-    <MobileSidebarController />
+    <div>
+      <MobileSidebarController />
+      <h3 class="font-bold text-xl">Today's Diary</h3>
+    </div>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
   </div>
 </template>
 
