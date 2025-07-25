e9afa9c5b3f0ffd4369256cbd40d08adf3b610a4
liangmiQwQ <mi_yyds@126.com>
2025-07-25 08:56:08 +0800

    feat: add dialog

--- app/components/sidebar-item-renderer.vue
+++ app/components/sidebar-item-renderer.vue
@@ -9,27 +9,71 @@
               {{ item.name }}
             </NuxtLink>
 
-            <!-- 配置详细按钮 -->
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Button size="icon" variant="ghost" class="cursor-pointer">
-                  <Icon name="proicons:more" class="text-xl" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuGroup>
-                  <DropdownMenuLabel>Edit</DropdownMenuLabel>
-                  <DropdownMenuItem>
-                    <Icon name="wpf:rename" class="text-lg" />
-                    <span>Rename</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Icon name="material-symbols:delete-outline-sharp" class="text-lg" />
-                    <span>Delete</span>
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <Dialog>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button size="icon" variant="ghost" class="cursor-pointer">
+                    <Icon name="proicons:more" class="text-xl" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuGroup>
+                    <DropdownMenuLabel>Edit</DropdownMenuLabel>
+
+                    <!-- DialogTriggers -->
+                    <DialogTrigger as-child>
+                      <DropdownMenuItem
+                        class="flex items-center gap-3"
+                        @click="setDialogType('rename')"
+                      >
+                        <span>Rename</span>
+                      </DropdownMenuItem>
+                    </DialogTrigger>
+
+                    <DialogTrigger as-child>
+                      <DropdownMenuItem
+                        class="flex items-center gap-3"
+                        @click="setDialogType('delete')"
+                      >
+                        <span>Delete</span>
+                      </DropdownMenuItem>
+                    </DialogTrigger>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              <DialogContent v-if="dialogType === 'rename'">
+                <DialogHeader>
+                  <DialogTitle>Rename</DialogTitle>
+                  <!-- Do Not Need Any description -->
+                </DialogHeader>
+                <div>
+                  <!-- Body -->
+                </div>
+                <DialogFooter>
+                  <DialogClose>
+                    <Button variant="secondary">Cancel</Button>
+                  </DialogClose>
+                  <Button type="submit"> Save </Button>
+                </DialogFooter>
+              </DialogContent>
+
+              <DialogContent v-else>
+                <DialogHeader>
+                  <DialogTitle>Delete</DialogTitle>
+                  <!-- Do Not Need Any description -->
+                </DialogHeader>
+                <div>
+                  <!-- Body -->
+                </div>
+                <DialogFooter>
+                  <DialogClose>
+                    <Button variant="secondary">Cancel</Button>
+                  </DialogClose>
+                  <Button type="submit"> Save </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </SidebarMenuButton>
         </SidebarMenuItem>
       </SidebarMenu>
@@ -39,11 +83,16 @@
 
 <script lang="ts" setup>
 import type { RenderingItem } from "~/types/sidebarRendering";
+const dialogType = ref<"rename" | "delete">("rename");
 
 const props = defineProps<{
   title: string;
   data: RenderingItem[];
 }>();
+
+const setDialogType = (type: "rename" | "delete") => {
+  dialogType.value = type;
+};
 </script>
 
 <style></style>
