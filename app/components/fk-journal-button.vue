bde8d83b30a150dc9f1cfda843d60ffb706c1069
liangmiQwQ <mi_yyds@126.com>
2025-07-27 02:37:10 +0800

    fix: fixed dialog rendering bug

--- app/components/fk-journal-button.vue
+++ app/components/fk-journal-button.vue
@@ -38,44 +38,46 @@ function submit() {
 </script>
 
 <template>
-  <Dialog>
-    <DialogTrigger :disabled="props.disabled">
-      <Button size="icon" class="!w-[30px] !h-[30px]" variant="outline" @click="clickHandle">
-        <Icon name="ic:round-plus" class="text-lg" />
-      </Button>
-    </DialogTrigger>
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>Add Journal</DialogTitle>
-        <DialogDescription>Add Journal to make AI can edit your journal data</DialogDescription>
-      </DialogHeader>
+  <ClientOnly>
+    <Dialog>
+      <DialogTrigger :disabled="props.disabled">
+        <Button size="icon" class="!w-[30px] !h-[30px]" variant="outline" @click="clickHandle">
+          <Icon name="ic:round-plus" class="text-lg" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add Journal</DialogTitle>
+          <DialogDescription>Add Journal to make AI can edit your journal data</DialogDescription>
+        </DialogHeader>
 
-      <Select v-model="selectedJournal">
-        <SelectTrigger class="w-full">
-          <SelectValue placeholder="Select a journal" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectItem v-for="(item, index) of renderingData" :key="item._id + index.toString()" :value="item._id">
-              {{ item.title }}
-            </SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+        <Select v-model="selectedJournal">
+          <SelectTrigger class="w-full">
+            <SelectValue placeholder="Select a journal" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem v-for="(item, index) of renderingData" :key="item._id + index.toString()" :value="item._id">
+                {{ item.title }}
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
 
-      <textarea :value="textValue" class="resize-none h-40" placeholder="Keep the sentence you want to update there" />
-      <DialogFooter>
-        <DialogClose class="flex gap-2">
-          <Button variant="secondary">
-            Cancel
-          </Button>
-          <Button :disabled="!(selectedJournal && textValue)" @click="submit">
-            Save
-          </Button>
-        </DialogClose>
-      </DialogFooter>
-    </DialogContent>
-  </Dialog>
+        <textarea :value="textValue" class="resize-none h-40" placeholder="Keep the sentence you want to update there" />
+        <DialogFooter>
+          <DialogClose class="flex gap-2">
+            <Button variant="secondary">
+              Cancel
+            </Button>
+            <Button :disabled="!(selectedJournal && textValue)" @click="submit">
+              Save
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  </ClientOnly>
 </template>
 
 <style>
